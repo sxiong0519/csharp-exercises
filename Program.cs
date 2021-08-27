76e73aa267b76079809d6310cea83f2ad7b8ec3c
Sue Xiong <sxiong0519@gmail.com>
2021-08-27 16:40:40 -0500

    completed

--- Program.cs
+++ Program.cs
@@ -9,18 +9,21 @@ namespace Classes
         {
             // Create an instance of a company. Name it whatever you like.
             Company friends = new Company("Friends", new DateTime(2012, 05, 09));
-            Console.WriteLine(friends.CreatedOn);
             // Create three employees
             Employee employee1 = new Employee("Joey", "Tribbiani", "CEO", new DateTime(2018, 08, 16));
             Employee employee2 = new Employee("Rachel", "Green", "Marketing Dir", new DateTime(2020, 09, 22));
             Employee employee3 = new Employee("Phoebe", "Buffary", "COO", new DateTime(2021, 01, 31));
-            Console.WriteLine(employee1.FirstName);
             // Assign the employees to the company
-
+            friends.employees.Add(employee1);
+            friends.employees.Add(employee2);
+            friends.employees.Add(employee3);
+            
             /*
                 Iterate the company's employee list and generate the
-                simple report shown above
+                simple report shown above (iteration is being done in company.cs file)
             */
+            friends.EmployeeInfo();
+            
         }
     }
 }
