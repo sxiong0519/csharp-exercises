01fb7d9c2c0c8c1058c8026ae2791ad19f81ffe6
Sue Xiong <sxiong0519@gmail.com>
2021-08-27 15:45:25 -0500

    created company

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Classes
 {
@@ -6,7 +7,17 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create an instance of a company. Name it whatever you like.
+            Company friends = new Company("Friends", DateTime.Parse("10/15/2012"));
+            Console.WriteLine(friends);
+            // Create three employees
+
+            // Assign the employees to the company
+
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
         }
     }
 }
