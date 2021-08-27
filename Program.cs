cc1e5797e376484c183335030c5622a5b6eea2da
Sue Xiong <sxiong0519@gmail.com>
2021-08-27 15:59:47 -0500

    added employees

--- Program.cs
+++ Program.cs
@@ -8,10 +8,13 @@ namespace Classes
         static void Main(string[] args)
         {
             // Create an instance of a company. Name it whatever you like.
-            Company friends = new Company("Friends", DateTime.Parse("10/15/2012"));
-            Console.WriteLine(friends);
+            Company friends = new Company("Friends", new DateTime(2012, 05, 09));
+            Console.WriteLine(friends.CreatedOn);
             // Create three employees
-
+            Employee employee1 = new Employee("Joey", "Tribbiani", "CEO", new DateTime(2018, 08, 16));
+            Employee employee2 = new Employee("Rachel", "Green", "Marketing Dir", new DateTime(2020, 09, 22));
+            Employee employee3 = new Employee("Phoebe", "Buffary", "COO", new DateTime(2021, 01, 31));
+            Console.WriteLine(employee1.FirstName);
             // Assign the employees to the company
 
             /*
